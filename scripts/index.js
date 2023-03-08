dad56e25e8f51cbdd053fa36d9b020128a445b81
nskmikhey <NskMikhei@yandex.ru>
2023-03-08 16:32:38 +0700

    начать писать скрипт

--- scripts/index.js
+++ scripts/index.js
@@ -1,1 +1,15 @@
-const popupElement = document.querySelector(".popup");
+const popupElement = document.querySelector('.popup');
+const popupCloseButtonElement = popupElement.querySelector('.popup__close-button');
+const popupOpenButtonElement = document.querySelector('.profile__edit-button');
+
+const openPopupVisibility = function () {
+  popupElement.classList.add('popup_is-opened')
+};
+
+const closePopup = function () {
+  popupElement.classList.remove('popup_is-opened')
+};
+
+popupOpenButtonElement.addEventListener('click', openPopupVisibility);
+popupCloseButtonElement.addEventListener('click', closePopup);
+
