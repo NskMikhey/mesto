8b3e6a34a872abab2929795b29372d6058db3c22
nskmikhey <NskMikhei@yandex.ru>
2023-03-09 14:18:04 +0700

    сделать логику сохранения

--- scripts/index.js
+++ scripts/index.js
@@ -1,15 +1,29 @@
 const popupElement = document.querySelector('.popup');
 const popupCloseButtonElement = popupElement.querySelector('.popup__close-button');
 const popupOpenButtonElement = document.querySelector('.profile__edit-button');
+const nameProfile = document.querySelector('.profile__name');
+const aboutProfile = document.querySelector('.profile__about');
+const nameInput = document.getElementById('profile_name');
+const aboutInput = document.getElementById('profile_about');
+const formElement = popupElement.querySelector('.popup__container');
 
-const openPopupVisibility = function () {
-  popupElement.classList.add('popup_is-opened')
+const openPopup = function () {
+  popupElement.classList.add('popup_is-opened');
+  nameInput.value = nameProfile.textContent;
+  aboutInput.value = aboutProfile.textContent;
 };
 
 const closePopup = function () {
-  popupElement.classList.remove('popup_is-opened')
+  popupElement.classList.remove('popup_is-opened');
 };
 
-popupOpenButtonElement.addEventListener('click', openPopupVisibility);
-popupCloseButtonElement.addEventListener('click', closePopup);
+const handleFormSubmit = function (evt) {
+  evt.preventDefault();
+  nameProfile.textContent = nameInput.value;
+  aboutProfile.textContent = aboutInput.value;
+  closePopup();
+}
 
+popupOpenButtonElement.addEventListener('click', openPopup);
+popupCloseButtonElement.addEventListener('click', closePopup);
+formElement.addEventListener('submit', handleFormSubmit);
