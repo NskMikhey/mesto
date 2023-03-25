ebaa45ada19398c86d7b1a06961efb7d3031710c
nskmikhey <NskMikhei@yandex.ru>
2023-03-25 16:43:25 +0700

    fix after review

--- scripts/index.js
+++ scripts/index.js
@@ -23,6 +23,10 @@ const editButton = document.querySelector('.profile__edit-button');
 const addButton = document.querySelector('.profile__add-button');
 const exitButtons = document.querySelectorAll('.popup__close-button');
 
+//массив с карточками
+const cardContainer = document.querySelector('.elements');
+const cardTemplate = document.querySelector('.card-template').content;
+
 //Добавить класс откр попап
 const openPopup = (popup) => {
   popup.classList.add('popup_is-opened');
@@ -51,37 +55,6 @@ const closePopup = () => {
 
 //Слушатель на закрытие
 exitButtons.forEach((exit) => exit.addEventListener('click', closePopup));
-
-
-//массив с карточками
-const cardContainer = document.querySelector('.elements');
-const cardTemplate = document.querySelector('.card-template').content;
-const initialCards = [
-  {
-    name: 'Архыз',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg',
-  },
-  {
-    name: 'Челябинская область',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg',
-  },
-  {
-    name: 'Иваново',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg',
-  },
-  {
-    name: 'Камчатка',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg',
-  },
-  {
-    name: 'Холмогорский район',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg',
-  },
-  {
-    name: 'Байкал',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg',
-  },
-];
 
 //Создание карточек
 function createCard(data) {
