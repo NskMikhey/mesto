3242fec1624c97e45942e540e3caec190bbe6762
nskmikhey <NskMikhei@yandex.ru>
2023-04-02 18:03:41 +0700

    add заготовка под валидацию

---
+++ scripts/validate.js
@@ -0,0 +1,17 @@
+//
+const enableValidationParam = {
+  formSelector: '.popup__form',
+  inputSelector: '.popup__input',
+  submitButtonSelector: '.popup__button',
+  inactiveButtonClass: 'popup__button_disabled',
+  inputErrorClass: 'popup__input_type_error',
+  errorClass: 'popup__error_visible'
+};
+
+const log = console.log;
+
+enableValidation(enableValidationParam);
+
+function enableValidation(param) {
+  
+}
